266bd076e5e97c1140d04f0659522fd66f30bba8
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 11:17:49 -0700

    vendor: soong: Add TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I925b137edeed9fa11bbe566002147b444328c661

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -7,10 +7,15 @@ type Product_variables struct {
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
 	}
+	Uses_qti_camera_device struct {
+		Cppflags    []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
 	Specific_camera_parameter_library     *string `json:",omitempty"`
 	Target_uses_color_metadata            *bool   `json:",omitempty"`
 	Uses_generic_camera_parameter_library *bool   `json:",omitempty"`
+	Uses_qti_camera_device                *bool   `json:",omitempty"`
 }
