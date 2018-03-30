730af5d56b54a2490d61b87918d66c59af88161a
Josh <joshfox87@gmail.com>
2018-03-30 08:50:53 -0400

    Themes: Add Black variant to styles [2/3]
    dark is nice but somethimes we want things black
    
    props to camcory for help with the theme
    
    update to pie
    
    Change-Id: I288de5c48d6e784ce0de31f6d418f045da0d715c

--- themes/themes.mk
+++ themes/themes.mk
@@ -2,9 +2,13 @@
 PRODUCT_PACKAGES += \
     GBoardDark \
     GBoardLight \
+    GBoardBlack \
     GoogleIntelligenceSenseDark \
     GoogleIntelligenceSenseLight \
     SettingsDark \
+    SettingsBlack \
     SettingsIntelligenceDark \
+    SettingsIntelligenceBlack \
     SystemDark \
+    SystemBlack \
     WellbeingDark
