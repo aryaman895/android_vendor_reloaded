dff38c59969dfc18ea352ee7f84255ed783e0ddb
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    vendor: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- config/BoardConfigCustom.mk
+++ config/BoardConfigCustom.mk
@@ -5,3 +5,8 @@ endif
 
 # Disable qmi EAP-SIM security
 DISABLE_EAP_PROXY := true
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
