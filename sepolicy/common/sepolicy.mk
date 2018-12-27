RevengeOS/android_vendor_revengeos

sepolicy: Move policies to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c
Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

sepolicy/common/sepolicy.mk
@@ -3,5 +3,9 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    vendor/citrus/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/citrus/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/citrus/sepolicy/common/private
+

aryaman895/android_vendor_reloaded

sepolicy/common/sepolicy.mk
@@ -1,7 +1,10 @@
 #
-# This policy configuration will be used by all products that
-# inherit from Lineage
+# This policy configuration will be used by all products
 #
 
-BOARD_SEPOLICY_DIRS += \
-    vendor/reloaded/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/reloaded/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/reloaded/sepolicy/common/private
+