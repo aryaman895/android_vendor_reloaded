Cardinal-AOSP/vendor_cardinal

vendor/sepolicy: Initial repo structure

sepolicy/common/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/cardinal/sepolicy/common

sepolicy/qcom/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/cardinal/sepolicy/qcom

aryaman895/android_vendor_reloaded

sepolicy/common/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/reloaded/sepolicy/common

sepolicy/qcom/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/reloaded/sepolicy/qcom