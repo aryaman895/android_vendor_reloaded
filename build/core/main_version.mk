1898c1ae00efaf9a5327af7b227102c738717654
Ritesh Saxena <riteshsax007@gmail.com>
2019-05-19 10:16:26 +0000

    vendor: Little changes to the versioning
    Change-Id: I33f4891c21eeb2760f737cee7b1a67d381debb8b

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,5 +1,5 @@
 # Apply it to build.prop
 ADDITIONAL_BUILD_PROPERTIES += \
-    ro.modversion=$(RELOADED_VERSION_PROP) \
+    ro.modversion=$(RELOADED_VERSION) \
     ro.reloaded.version=$(RELOADED_VERSION_PROP) \
     ro.caf.version=$(shell grep "<default revision=" .repo/manifest.xml | awk -F'"' '{print $$2}' | awk  -F "/" '{print $$3}')
