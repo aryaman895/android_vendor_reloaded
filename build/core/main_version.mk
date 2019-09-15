6940e3655632e383c1d1c8ff6b0274cf67bfa0ac
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-15 18:54:45 +0300

    build: set build fingerprint for all devices
    * there's no per device way to add props to /system/build.prop
    * adding it here is the only way so far
    
    Change-Id: Iafc286d9e479677540a4ec617db76715c018ae8e

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,4 +1,8 @@
-# Apply it to build.prop
+# Build fingerprint
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+# ReloadedOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.modversion=$(RELOADED_VERSION) \
     ro.reloaded.version=$(RELOADED_VERSION_PROP) \
